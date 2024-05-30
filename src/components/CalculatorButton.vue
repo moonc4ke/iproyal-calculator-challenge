a631fa94b6e6aa3fcc3a58e7d8b9268c292cb2d0
mooncake <adom.donatas@gmail.com>
2024-05-30 19:10:09 +0300

    Add styles for CalculatorResult component and create reusable CalculatorButton component

---
+++ src/components/CalculatorButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="button" @click="handleClick" @keydown.enter="handleClick">
+    {{ title }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+defineProps<{
+  title: string;
+}>();
+
+const emit = defineEmits<{
+  (e: 'click'): void;
+}>();
+
+const handleClick = () => {
+  emit('click');
+};
+</script>
+
+<style scoped>
+.button {
+  cursor: pointer;
+  background-color: #5A5757;
+  color: #fff;
+  box-shadow: 5px 5px 40px 0px #0B0B0B4D;
+  border-radius: 10px;
+  display: flex;
+  padding: 10px;
+  align-items: center;
+}
+</style>
--- src/components/CalculatorResult.vue
+++ src/components/CalculatorResult.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { inject, type Ref } from 'vue';
 import CalculatorHistory from './CalculatorHistory.vue';
+import CalculatorButton from './CalculatorButton.vue';
 
 const result = inject<Ref<number | null>>('result');
 const calculate = inject<() => void>('calculate');
@@ -10,8 +11,46 @@ const errorMessage = inject<Ref<string>>('errorMessage');
 <template>
   <div>
     <CalculatorHistory />
-    <button @click="calculate" type="button">Calculate</button>
-    <p v-if="errorMessage">{{ errorMessage }}</p>
-    <p>Result: {{ result }}</p>
+
+    <section class="calculator-result-section">
+      <p class="error-message" v-if="errorMessage">{{ errorMessage }}</p>
+      <div class="calculator-result-section-container">
+        <CalculatorButton title="Calculate" @click="calculate" />
+        <div class="calculator-result-section-container-display">
+          <span class="calculator-result-section-container-display-equal">=</span>
+          <span>{{ result }}</span>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<style scoped>
+.calculator-result-section {
+  margin-top: 40px;
+}
+
+.calculator-result-section .error-message {
+  color: #C15D60;
+}
+
+.calculator-result-section-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.calculator-result-section-container-display {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  min-width: 5rem;
+  background-color: #5A5757;
+  box-shadow: 5px 5px 60px 0px #0B0B0B1A;
+  border-radius: 10px;
+}
+
+.calculator-result-section-container-display-equal {
+  margin-right: 4px;
+}
+</style>
